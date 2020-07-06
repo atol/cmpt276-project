3249ff08c459d8d6ba27a41ac7df54fbabe08b9d
MCHOI <MCHOI@MacBook-Pro-Mike-77.local>
2020-07-05 21:08:52 -0700

    changed the homepage

--- public/scriptHomePage.js
+++ public/scriptHomePage.js
@@ -2,3 +2,11 @@
 $(document).ready(function () {
   $('.header').height($(window).height());
 })
+
+function openlogin() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeLogin() {
+  document.getElementById("myForm").style.display = "none";
+}
