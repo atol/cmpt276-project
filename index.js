77ecb1fcbae83fa78a8386ea9a050cf1062157dc
Hassan Shahid <hassans@sfu.ca>
2020-07-07 19:39:23 -0500

    changes

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const PORT = process.env.PORT || 5000
 const volleyball = require('volleyball')
 const auth = require('./auth')
 
-<<<<<<< HEAD
 const app=express()
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
@@ -16,12 +15,3 @@ app.get('/', (req, res) => res.redirect('/public/index.html'))
 app.use('/auth',auth) //prepends things in auth/index.js with /auth
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
-=======
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
-
->>>>>>> origin/mikeBranch
