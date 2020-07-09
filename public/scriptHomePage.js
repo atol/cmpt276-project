a3d6a15d68e5f51b2c5b7a09b20d34aba1477560
Michael Choi <michaelchoi97@gmail.com>
2020-07-09 14:31:53 -0700

    added error message for when user inputs wrong email or password

--- public/scriptHomePage.js
+++ public/scriptHomePage.js
@@ -3,3 +3,23 @@ $(document).ready(function () {
   $('.header').height($(window).height());
 })
 
+
+// function login() {
+//   var data = {};
+//   data.email = document.getElementById("email");
+//   data.password = document.getElementById("pwd");
+//   $.ajax({
+//     type: 'POST',
+//     data: JSON.stringify(data),
+//     cache: false,
+//     contentType: 'application/json',
+//     datatype: "json",
+//     url: '/',
+//     success: function (returns) {
+//       if (returns)
+//         alert("User save");
+//       else
+//         alert("Error: user not save");
+//     }
+//   });
+// }
