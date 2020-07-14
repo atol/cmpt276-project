45cfcd4a9abb0e46f102ea93c28f751d49f08269
keitolee <keitolee96@gmail.com>
2020-07-13 23:35:02 -0700

    created infoMapScript.js

---
+++ public/infoMapScript.js
@@ -0,0 +1,14 @@
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "300px";
+    console.log()
+}
+  
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+}
+
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2VpdG9sZWUiLCJhIjoiY2tjOG0wbXhuMGN1ZTM0cGZ1d28zcWx6cyJ9.oSR2bnxW0qtkQ3YM8pBRZg';
+
