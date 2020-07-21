cf21f5f6716fe8c980883877a965e0f6761bec38
Michael Choi <michaelchoi97@gmail.com>
2020-07-21 15:09:05 -0700

    added iterneries.ejs

--- index.js
+++ index.js
@@ -21,17 +21,17 @@ app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(volleyball)
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(session({
     secret: 'shhhhhhhh', // sign session ID cookie
     resave: false,
     saveUninitialized: true
 }))
-app.use('/auth',auth) //prepends things in auth/index.js with /auth
+app.use('/auth', auth) //prepends things in auth/index.js with /auth
 
 app.get('/', function (req, res) {
     var user = req.session.user_id;
-    res.render('pages/index', {logged_in: user});
+    res.render('pages/index', { logged_in: user });
 });
 
 app.get('/login', (req, res) => res.redirect('/login.html'))
@@ -42,15 +42,19 @@ app.get('/logout', function (req, res) {
     req.session.destroy();
     res.redirect('/logout.html');
 });
-
+//for mytravel information
+app.get('/myTravel', checkAuth, (req, res) => {
+    var name = req.session.uname;
+    res.render('pages/myTravel', { uname: name })
+})
 app.get('/dashboard', checkAuth, function (req, res) {
     var name = req.session.uname;
-    res.render('pages/basic_user', {uname: name});
+    res.render('pages/basic_user', { uname: name });
 });
 
 app.get('/mod', checkAuth, checkRole(ACCESS.MOD), function (req, res) {
     var name = req.session.uname;
-    res.render('pages/mod', {uname: name});
+    res.render('pages/mod', { uname: name });
 });
 
 app.get('/mod_mail', checkAuth, checkRole(ACCESS.MOD), function (req, res) {
@@ -59,31 +63,31 @@ app.get('/mod_mail', checkAuth, checkRole(ACCESS.MOD), function (req, res) {
 
 app.get('/admin', checkAuth, checkRole(ACCESS.ADMIN), function (req, res) {
     var name = req.session.uname;
-    res.render('pages/admin', {uname: name});
+    res.render('pages/admin', { uname: name });
 });
 
 app.get('/users', checkAuth, checkRole(ACCESS.ADMIN), function (req, res) {
-  var id = 0;
-  var getAllUser = 'SELECT * FROM users where accesslevel = ($1)';
-  pool.query(getAllUser,[id], (error,result) => {
-    if(error){
-      res.end(error);
-    }
-    var results = {'rows':result.rows};
-    res.render('pages/users', results);
-  });
+    var id = 0;
+    var getAllUser = 'SELECT * FROM users where accesslevel = ($1)';
+    pool.query(getAllUser, [id], (error, result) => {
+        if (error) {
+            res.end(error);
+        }
+        var results = { 'rows': result.rows };
+        res.render('pages/users', results);
+    });
 });
 
 app.get('/allmods', checkAuth, checkRole(ACCESS.ADMIN), function (req, res) {
-  var id = 1;
-  var getAllUser = 'SELECT * FROM users where accesslevel = ($1)';
-  pool.query(getAllUser,[id], (error,result) => {
-    if(error){
-      res.end(error);
-    }
-    var results = {'rows':result.rows};
-    res.render('pages/allmods', results);
-  });
+    var id = 1;
+    var getAllUser = 'SELECT * FROM users where accesslevel = ($1)';
+    pool.query(getAllUser, [id], (error, result) => {
+        if (error) {
+            res.end(error);
+        }
+        var results = { 'rows': result.rows };
+        res.render('pages/allmods', results);
+    });
 });
 
 function checkAuth(req, res, next) {
