652f3faf52dab2b7ef4c8bebc77da050131d3dfa
Hassan Shahid <hassans@sfu.ca>
2020-07-23 15:58:25 -0500

    finished system

--- friends.js
+++ friends.js
@@ -12,13 +12,13 @@ router.get('/', checkAuth, async (req, res) => {
     //console.log(user_id)
     try {
         const client = await pool.connect();
-        let qResult = await client.query(`select name from users where id IN (select user_b from friends where user_a=$1 UNION select user_a from friends where user_b=$1)`, [user_id])
+        let qResult = await client.query(`select id,name,email from users where id IN (select user_b from friends where user_a=$1 UNION select user_a from friends where user_b=$1)`, [user_id])
         if (qResult.rows && qResult.rows.length > 0) {
             var results = {'rows':qResult.rows};
             res.render('pages/friends', results);
         }
         else {
-            res.send("You have no friends :( How sad")
+            res.render('pages/noFriends');
             }
         client.release()
     } catch (err) {
@@ -77,6 +77,21 @@ router.post('/add',checkAuth, async (req,res)=>{
     }
 })
 
+router.post('/delete/:id', checkAuth, async (req, res) => {
+    const user_id = req.session.user_id
+    const otherUser_id=req.params.id
+    try {
+        const client = await pool.connect();
+        await client.query(`delete from friends where (user_a=$1 and user_b=$2) OR (user_a=$2 and user_b=$1)`, [user_id,otherUser_id])
+        res.send("Friend successfully deleted.")
+        client.release()
+    } catch (err) {
+        console.error(err)
+        res.send("err")
+    }
+})
+
+
 router.get('/friendreqs/sent', checkAuth, async (req, res) => {
     const user_id = req.session.user_id
     //console.log(user_id)
@@ -101,7 +116,7 @@ router.get('/friendreqs/recieved', checkAuth, async (req,res)=>{
     const user_id= req.session.user_id
     try {
         const client = await pool.connect();
-        let qResult = await client.query(`select name,email from users where id IN (select from_User from friendreqs where to_User=$1 )`, [user_id])
+        let qResult = await client.query(`select id,name,email from users where id IN (select from_User from friendreqs where to_User=$1 )`, [user_id])
         if (qResult.rows && qResult.rows.length > 0) {
             var results = {'rows':qResult.rows};
             res.render('pages/friendReqsRecieved', results);
@@ -116,6 +131,37 @@ router.get('/friendreqs/recieved', checkAuth, async (req,res)=>{
     }
 })
 
+router.post('/friendreqs/accept/:id',checkAuth, async (req,res)=>{
+    const toUser_id= req.session.user_id
+    const fromUser_id= req.params.id
+    try {
+        const client = await pool.connect();
+        await client.query(`delete from friendreqs where from_user=$1 and to_user=$2`, [fromUser_id,toUser_id])
+        try {
+            await client.query(`insert into friends( user_a, user_b) values ($1,$2)`, [toUser_id,fromUser_id])
+            client.release()
+            res.send("Friend request accepted!")
+        } catch (err) {
+            console.error(err)
+            res.send("err")
+        }
+    } catch (err) {
+        console.error(err)
+        res.send("err")
+    }
+})
+router.post('/friendreqs/decline/:id',checkAuth, async (req,res)=>{
+    const toUser_id= req.session.user_id
+    const fromUser_id= req.params.id
+    try {
+        const client = await pool.connect();
+        await client.query(`delete from friendreqs where from_user=$1 and to_user=$2`, [fromUser_id,toUser_id])
+        res.send("Deleted friend request.")
+    } catch (err) {
+        console.error(err)
+        res.send("err")
+    }
+})
 
 function checkAuth(req, res, next) {
     if (!req.session.user_id) {
