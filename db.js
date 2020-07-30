08051f1db878a84dd07aa69d9989f63af5074bc2
Hassan Shahid <hassans@sfu.ca>
2020-07-30 18:38:35 -0500

    fixed db...again/

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 var pool = new Pool({
-  //connectionString: process.env.DATABASE_URL
-  connectionString: 'postgres://postgres:root@localhost/authdb'
+  connectionString: process.env.DATABASE_URL
+  //connectionString: 'postgres://postgres:root@localhost/authdb'
 })
 
 exports.pool = pool
