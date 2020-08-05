c05f58a6114a89306daaa8e10eae4577d2f8fb5f
atol <alicelinlc@gmail.com>
2020-08-05 10:55:16 -0700

    Add map mock test

---
+++ test/map-test.js
@@ -0,0 +1,23 @@
+const createGoogleMapsMock = require('./src/createGoogleMapsMock')
+
+describe('createGoogleMapsMock', () => {
+    let googleMaps;
+
+    beforeEach(() => {
+        googleMaps = createGoogleMapsMock();
+    });
+
+    it('should create a map mock', () => {
+        const mapDiv = document.createElement('div');
+        new googleMaps.Map(mapDiv);
+
+        // The function was called exactly once
+        expect(googleMaps.Map).toHaveBeenCalledTimes(1);
+
+        // The function was instantiated exactly once
+        expect(googleMaps.Map.mock.instances.length).toBe(1);
+
+        // The last call to the function was called with the 'mapDiv' argument
+        expect(googleMaps.Map).toHaveBeenLastCalledWith(mapDiv);
+    });
+});
