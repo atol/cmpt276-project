98a4601d774940801e0107637afc8788c9476b8b
Hassan Shahid <hassans@sfu.ca>
2020-08-06 17:32:36 -0500

    styles!

--- reviews.js
+++ reviews.js
@@ -11,10 +11,10 @@ router.get('/', checkAuth, async(req, res) => {
         let qResult=await client.query(`select * from reviews where writer_id=$1`,[user_id])
         if (qResult.rows && qResult.rows.length > 0) {
             var results = {'rows':qResult.rows};
-            res.render('pages/myReviews', results);
+            res.render('pages/myReviews', {logged_in:user_id,'rows':qResult.rows});
         }
         else{
-            res.render('pages/reviewsResponse',{message:"You haven't written any reviews."})
+            res.render('pages/reviewsResponse',{logged_in:user_id,message:"You haven't written any reviews."})
         }
         client.release()
     } catch (err) {
@@ -22,14 +22,19 @@ router.get('/', checkAuth, async(req, res) => {
         res.send("err")
     }
 })
+router.get('/invalidurl', checkAuth, async(req, res) => {
+    const user_id=req.session.user_id
+    res.render('pages/reviewsResponse',{logged_in:user_id,message:"Please select a Country before writing or viewing a review!"})
+})
 
 router.post('/delete/:review_id', checkAuth, async(req, res) => {
+    var user_id = req.session.user_id
     const review_id=req.params.review_id
     try {
         const client = await pool.connect();
         await client.query(`delete from reviews where review_id=$1`,[review_id])
         client.release()
-        res.render('pages/reviewsResponse',{message:"The selected review has been deleted"})
+        res.render('pages/reviewsResponse',{logged_in:user_id,message:"The selected review has been deleted"})
     } catch (err) {
         console.error(err)
         res.send("err")
@@ -37,8 +42,9 @@ router.post('/delete/:review_id', checkAuth, async(req, res) => {
 })
 
 router.get('/write/:countryName', checkAuth, (req, res) => {
+    var user_id = req.session.user_id
     const input_country = req.params.countryName
-    res.render('pages/writeReview',{country:input_country})
+    res.render('pages/writeReview',{logged_in:user_id,country:input_country})
 })
 
 router.post('/:countryName', checkAuth, async(req, res) => {
@@ -55,7 +61,7 @@ router.post('/:countryName', checkAuth, async(req, res) => {
             `insert into reviews (writer_id,country,review_title,startdate,enddate,location,description) values($1,$2,$3,$4,$5,$6,$7)`,
             [writer_id,country,review_title,startdate,enddate,location,description])
         client.release()
-        res.render('pages/reviewsResponse',{message:"Review added!"})
+        res.render('pages/reviewsResponse',{logged_in:writer_id,message:"Review added!"})
     } catch (err) {
         console.error(err)
         res.send("err")
@@ -63,17 +69,17 @@ router.post('/:countryName', checkAuth, async(req, res) => {
 })
 
 router.get('/:countryName', checkAuth, async(req, res) => {
+    var user = req.session.user_id
     const country = req.params.countryName
     try {
         const client = await pool.connect();
         let qResult = await client.query(`select * from reviews where country=$1`, [country])
         if (qResult.rows && qResult.rows.length > 0) {
             var results = {'rows':qResult.rows};
-            res.render('pages/reviews', results);
+            res.render('pages/reviews', {logged_in: user,'rows':qResult.rows});
         }
         else {
-            res.render('pages/reviewsResponse',{message:"No one has written a review for this country yet"})
-            //res.render('pages/noFriends');
+            res.render('pages/reviewsResponse',{logged_in:user,message:"No one has written a review for this country yet"})
             }
         client.release()
     } catch (err) {
