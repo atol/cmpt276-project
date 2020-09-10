bbd0565ead3397fb79c35194b19d8e110860bf30
atol <alicelinlc@gmail.com>
2020-09-09 21:11:25 -0700

    Fix news articles retrieval

--- routes/news.js
+++ routes/news.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const fetch = require('node-fetch');
 
 router.get('/', async (req, res) => {
     const user = req.session.user_id;
